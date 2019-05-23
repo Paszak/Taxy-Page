6ed7473585699b2700a1a1ee57e153cba071771c
Paweł Koleśnik <pawelkolesnik@wp.pl>
2019-05-23 14:13:09 +0200

    try add yoastseo

--- js/scripts.js
+++ js/scripts.js
@@ -18,10 +18,10 @@
 //   });
 // });
 
-navbarSupportedContent
 
 $(document).ready(function() {
 	$('.nav-link').click(function() {
 		$('#navbarSupportedContent').removeClass('show');
 	});
 });
+
