ae795ffd9adc40d1c0e1a4e035c97ae1cc42779c
Paweł Koleśnik <pawelkolesnik@wp.pl>
2019-06-14 12:41:40 +0200

    add 301 redirect

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,4 @@
-// $(document).ready(function() {
-//   //change the integers below to match the height of your upper dive, which I called
-//   //banner.  Just add a 1 to the last number.  console.log($(window).scrollTop())
-//   //to figure out what the scroll position is when exactly you want to fix the nav
-//   //bar or div or whatever.  I stuck in the console.log for you.  Just remove when
-//   //you know the position.
-//   $(window).scroll(function () { 
-
-//     console.log($(window).scrollTop());
-
-//     if ($(window).scrollTop() > 80) {
-//       $('#navigation').addClass('fixed-top');
-//     }
-
-//     if ($(window).scrollTop() < 81) {
-//       $('#navigation').removeClass('fixed-top');
-//     }
-//   });
-// });
-
+window.location !== "https://mercedestaxigoleniow.pl" ? window.location = "https://mercedestaxigoleniow.pl" : true;
 
 $(document).ready(function () {
     $('.nav-link').click(function () {
