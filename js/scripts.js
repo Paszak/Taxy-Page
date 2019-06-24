a3918147762af5c3637ce0987fee923833abab46
Paweł Koleśnik <pawelkolesnik@wp.pl>
2019-06-24 10:23:58 +0200

    change pictures format

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,3 @@
-window.location !== "https://mercedestaxigoleniow.pl" ? window.location = "https://mercedestaxigoleniow.pl" : true;
-
 $(document).ready(function () {
     $('.nav-link').click(function () {
         $('#navbarSupportedContent').removeClass('show');
